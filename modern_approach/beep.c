4e1e2ef3ba477efc14e98acc7f0283ad4e091bb1
Andriy Levitskyy <levitskyyandriy@gmail.com>
2024-12-16 13:23:34 -0500

    Finished second chapter

---
+++ modern_approach/beep.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int main(void) {
+    system("afplay /System/Library/Sounds/Glass.aiff");
+    system("afplay /System/Library/Sounds/Glass.aiff");
+    system("afplay /System/Library/Sounds/Glass.aiff");
+    system("afplay /System/Library/Sounds/Glass.aiff");
+    return 0;
+}
